3156a981e57a7facbab115237b6ca7989ae7ee78
Alexander Michael <support@alexandermichael.co.ke>
2017-08-26 18:09:05 +0300

    enabled AJAX on edit account form

--- crmapp/static/js/new-age.js
+++ crmapp/static/js/new-age.js
@@ -36,3 +36,15 @@
   });
 
 })(jQuery); // End of use strict
+
+// Main App
+$(document).ready(function() {
+
+    // Account - Use AJAX to get the Account Edit form and
+    // display it on the page w/out a refresh
+    $('#gi-container').delegate('.edit-account', 'click', function(e) {
+        e.preventDefault();
+        $('#gi-container').load($(this).attr('href'));
+    });
+
+});
