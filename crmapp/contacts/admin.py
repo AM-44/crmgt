0b5a8ecf89db06de52470f21d8dfc469e8d0b07a
Alexander Michael <support@alexandermichael.co.ke>
2017-08-26 18:58:50 +0300

    created the contacts model

--- crmapp/contacts/admin.py
+++ crmapp/contacts/admin.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'uuid')
+
+admin.site.register(Contact, ContactAdmin)
