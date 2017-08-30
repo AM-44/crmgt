745703289bb35dcf02f9a3874b610affe76eec12
Alexander Michael <support@alexandermichael.co.ke>
2017-08-30 13:13:52 +0300

    added search function to the currency list page

--- crmapp/currencies/views.py
+++ crmapp/currencies/views.py
@@ -7,12 +7,22 @@ from .models import Currency
 
 class CurrencyList(ListView):
     model = Currency
-    paginator = Paginator(Account, 12)
+    paginator = Paginator(Currency, 12)
     template_name = 'currencies/currency_list.html'
     context_object_name = 'currencies'
 
     def get_queryset(self):
-        currency_list = Currency.objects.filter(owner=self.request.user)
+        try:
+            a = self.request.GET.get('currency',)
+        except KeyError:
+            a = None
+        if a:
+            currency_list = Currency.objects.filter(
+                name__icontains=a,
+                owner=self.request.user
+            )
+        else:
+            currency_list = Currency.objects.filter(owner=self.request.user)
         return currency_list
 
     @method_decorator(login_required)
