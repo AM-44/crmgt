4c2e0e9f3dee99fb6098dd9bd853ed19c315b2ad
Alexander Michael <support@alexandermichael.co.ke>
2017-08-30 13:05:19 +0300

    created the currency list view

--- crmapp/currencies/views.py
+++ crmapp/currencies/views.py
@@ -1,6 +1,18 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-from django.shortcuts import render
+from .models import Currency
 
-# Create your views here.
+class CurrencyList(ListView):
+    model = Currency
+    template_name = 'currencies/currency_list.html'
+    context_object_name = 'currencies'
+
+    def get_queryset(self):
+        currency_list = Currency.objects.filter(owner=self.request.user)
+        return currency_list
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(CurrencyList, self).dispatch(*args, **kwargs)
