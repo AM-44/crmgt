ac3ab52e53b19dd4a9b25826583ac9164eee3566
Alexander Michael <support@alexandermichael.co.ke>
2017-08-30 13:00:35 +0300

    created the currencies model

--- crmapp/currencies/models.py
+++ crmapp/currencies/models.py
@@ -1,6 +1,35 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from shortuuidfield import ShortUUIDField
+
+
+class Currency(models.Model):
+    uuid = ShortUUIDField(unique=True)
+    currency_name = models.CharField(max_length=80)
+    desc = models.TextField(blank=True)
+    address_one = models.CharField(max_length=200)
+    address_two = models.CharField(max_length=200, blank=True)
+    exchange = models.CharField(max_length=50)
+    investment = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    owner = models.ForeignKey(User)
+    created_on = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'currencies'
+
+    def __unicode__(self):
+        return u"%s" % self.currency_name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return 'currency_detail', [self.uuid]
+
+    @models.permalink
+    def get_update_url(self):
+        return 'currency_update', [self.uuid]
+
+    @models.permalink
+    def get_delete_url(self):
+        return 'account_delete', [self.uuid]
